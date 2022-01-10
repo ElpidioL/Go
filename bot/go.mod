fd253b1ea680a14f1f6d7790e187231fed22c3cb
ElpidioL <lomeuelpidio@gmail.com>
2022-01-10 11:07:50 -0300

    0.1.1

--- bot/go.mod
+++ bot/go.mod
@@ -5,11 +5,9 @@ go 1.17
 require (
 	github.com/bwmarrin/discordgo v0.23.2
 	github.com/joho/godotenv v1.4.0
-	github.com/jonas747/dca v0.0.0-20210930103944-155f5e5f0cc7
 )
 
 require (
-	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
 )
--- bot/main.go
+++ bot/main.go
@@ -1,28 +1,30 @@
 package main
 
-import ( "fmt"
-		"github.com/bwmarrin/discordgo"
-		"bots/GOing/GObot"
-		"bots/GOing/options" 
+import (
+	"bots/GOing/GObot"
+	"bots/GOing/options"
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 var BotID string
 var Discord *discordgo.Session
 
+func main() {
 
-func main(){
 	// Create a new Discord session using the provided bot token.
 	// if it fail to get discord from discordgo, then it will not be nill and will stop
 	Discord, errDisc := discordgo.New("Bot " + options.Key)
 
 	if errDisc != nil {
-		fmt.Println("error creating Discord session,", errDisc)    
+		fmt.Println("error creating Discord session,", errDisc)
 		return
 	}
 	user, err := Discord.User("@me")
-	if err != nil{
-		fmt.Println("error setting Discord user,", errDisc)    
-		return 
+	if err != nil {
+		fmt.Println("error setting Discord user,", errDisc)
+		return
 	}
 	BotID = user.ID
 	//everything is ok, time to get started
